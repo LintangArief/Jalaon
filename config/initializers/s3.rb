8d27c5499aba2edd6867e37f0959e0de651e265b
Arip 41studio <arip@41studio.com>
2015-06-02 11:22:33 +0700

    add notification

--- config/initializers/s3.rb
+++ config/initializers/s3.rb
@@ -1,20 +1,3 @@
-# CarrierWave.configure do |config|
-#   config.cache_dir = "#{Rails.root}/tmp/" 
-#   config.permissions = 0666
-#   config.storage = :fog
-#   config.fog_credentials = {
-#     :provider               => 'AWS',
-#     :aws_access_key_id      => "AKIAIMGUFKYQ5HPC2PLA",
-#     :aws_secret_access_key  => "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",
-#     :region  => 'us-east-1'
-#   }
-#   config.fog_directory  = "cgmeetup"                             # required
-#   # config.fog_host     = 'https://assets.example.com'             # optional, defaults to nil
-#   # config.fog_public     = false                                  # optional, defaults to true
-#   config.fog_attributes = {'Cache-Control' => 'max-age=315576000'}  # optional, defaults to {}
-# end
-
-
 CarrierWave.configure do |config|
   config.cache_dir = "#{Rails.root}/tmp/" 
   config.permissions = 0666
@@ -25,6 +8,8 @@ CarrierWave.configure do |config|
     :aws_secret_access_key  => "kfA0821vm6exE+BepduwEK7Li+844fAPcZ2PB+sb",
     :region  => 'us-east-1'
   }
-  config.fog_directory  = "jalaon" 
-  config.fog_attributes = {'Cache-Control' => 'max-age=315576000'}
+  config.fog_directory  = "cgmeetup"                             # required
+  # config.fog_host     = 'https://assets.example.com'             # optional, defaults to nil
+  # config.fog_public     = false                                  # optional, defaults to true
+  config.fog_attributes = {'Cache-Control' => 'max-age=315576000'}  # optional, defaults to {}
 end
