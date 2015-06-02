f8f5224a802f7fe8a3d32f21f485b5f406cc6a97
Arip 41studio <arip@41studio.com>
2015-06-02 11:03:48 +0700

    add thin

--- config/initializers/Jalaon.rb
+++ config/initializers/Jalaon.rb
@@ -1,7 +1,7 @@
 if Rails.env == "production"
   ASSET_STORAGE = :fog
-  SITEMAP_ADAPTER = SitemapGenerator::S3Adapter.new(fog_directory: "jalaon")
+  # SITEMAP_ADAPTER = SitemapGenerator::S3Adapter.new(fog_directory: "jalaon")
 else
   ASSET_STORAGE = :file
-  SITEMAP_ADAPTER = SitemapGenerator::FileAdapter.new
+  # SITEMAP_ADAPTER = SitemapGenerator::FileAdapter.new
 end
